6f12495078081fe39ec38d18a882899dec55d1fc
apgerasimov <ap.gerasimov@gmail.com>
2019-10-22 18:51:09 +0300

    ТЕст на прод

---
+++ Python/stepik_language/test_items.py
@@ -0,0 +1,5 @@
+def test_online_store(browser):
+    browser.get('http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/')
+    button = browser.find_element_by_css_selector('#add_to_basket_form > button')
+    button = button.text
+    assert button == 'Додати в кошик'
